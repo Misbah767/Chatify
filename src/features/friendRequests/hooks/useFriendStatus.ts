efbd3751f6a167d4e8829f227c2b04cc9e5732e6
Misbah <hafizamisbah165@gmail.com>
2026-03-16 22:41:54 +0500

    fix: friend status selector error in useFriendStatus hook

--- src/features/friendRequests/hooks/useFriendStatus.ts
+++ src/features/friendRequests/hooks/useFriendStatus.ts
@@ -4,14 +4,15 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchFriendStatus } from "@/redux/actions/friendRequest.actions";
 import {
-  selectRelationshipStatus,
+  selectRelationshipStatusByUser,
   selectFriendRequestLoading,
 } from "@/redux/slices/friendRequest.slice";
 import { AppDispatch } from "@/redux/store";
 
 export const useFriendStatus = (userId: string) => {
   const dispatch = useDispatch<AppDispatch>();
-  const status = useSelector(selectRelationshipStatus);
+
+  const status = useSelector(selectRelationshipStatusByUser(userId));
   const loading = useSelector(selectFriendRequestLoading);
 
   useEffect(() => {
